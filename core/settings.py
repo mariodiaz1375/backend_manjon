4fb529ac2845dff3e05a201228828c059e83d6f0
mario <mariodiaz1375@gmail.com>
2026-02-27 18:14:44 -0300

    se agrego EMAIL_USE_SSL = True en settings

--- core/settings.py
+++ core/settings.py
@@ -204,8 +204,9 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'consultorio.manjon@gmail.com'
 EMAIL_HOST_PASSWORD = 'ufkl bfaj lrrj mbbj'
 DEFAULT_FROM_EMAIL = 'consultorio.manjon@gmail.com'
